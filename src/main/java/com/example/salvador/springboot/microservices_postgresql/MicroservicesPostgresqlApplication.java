ae0587fb63b4117537d3676b62ad4c61f4ca3da6
Jose Castro <jsalvadorca90@gmail.com>
2025-06-12 22:31:47 -0500

    feat: INICIO. [009] Creación proyecto, banner, colores consola.

---
+++ src/main/java/com/example/salvador/springboot/microservices_postgresql/MicroservicesPostgresqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.salvador.springboot.microservices_postgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesPostgresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesPostgresqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/salvador/springboot/microservices_postgresql/MicroservicesPostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.salvador.springboot.microservices_postgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesPostgresqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
